4fb73def50638c43fb19a32b7eb6b3346d4bf83d
railgaadi <railgaadi420@gmail.com>
2020-09-14 12:58:56 +0530

    add frontend build

---
+++ build/precache-manifest.05adb6232ab204571b81aa744e3a05ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef1ba45221533ec0d1c88b3dffacaf98",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a234549b6a4e8f6727f5",
+    "url": "/static/css/main.4539e68c.chunk.css"
+  },
+  {
+    "revision": "54dd32709c192453a110",
+    "url": "/static/js/2.d98781c2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d98781c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a234549b6a4e8f6727f5",
+    "url": "/static/js/main.d29d5c28.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
